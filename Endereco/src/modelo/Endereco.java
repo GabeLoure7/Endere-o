03b7a30d456baa492ea8a0a003b29a92c0362627
Administrador <Administrador@10.10.4.5>
2019-08-01 19:40:26 -0300

    erro

---
+++ Endereco/src/modelo/Endereco.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Endereco {
+private String Bairro;
+private String Complemento;
+private String Logradouro;
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    public String getLogradouro() {
+        return Logradouro;
+    }
+
+    public void setLogradouro(String Logradouro) {
+        this.Logradouro = Logradouro;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo{" + "Logradouro=" + Logradouro + '}';
+    }
+    
+
+}
